7b9c1a180405a01d32d6917cb6409a38b92bb781
jwgatt <jonnyliggatt@gmail.com>
2022-06-17 02:09:18 +0200

    missing html tags and require error

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-function App(){ 
-	return <div>Hello world</div>
+function App() {
+	const [displayTime, setDisplayTime] = React.useState(25 * 60)
+
+	const formatTime = (time) => {
+		let minutes = Math.floor(time / 60);
+		let seconds = time % 60;
+		return (minutes < 10 ? '0' + minutes : minutes) +
+			':' +
+			(seconds < 10 ? '0' + seconds : seconds)
 	}
 
-ReactDOM.render(<App/)>, document.getElementById('root'))
+	// display remaining time until break
+	return (
+		<div>
+			<Length title={'break length'} changetime={null} type={'break'} time={null} formattime />
+			<h1>{formatTime(displayTime)}</h1>
+		</div>
+	)
+
+	// Fixed missing html tags and a dep calling `require` error, compiler perhaps not recognising babel as a module. Adding length function to change break duration.
+	// ESLint not autofilling quotes in className
+	function Length({ title, changetime, type, time, formattime }) {
+		return (
+			<div>
+				<h3>{title}</h3>
+				<div className='time-sets'>
+					<button className='placeholder'>
+						<i className='placeholder'></i>
+					</button>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
