275bd6d9f5f8946b0ee1c3ac67fe36fa2bbaaac0
jwgatt <jonnyliggatt@gmail.com>
2022-06-18 17:05:01 +0200

    give arrow symbol functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function App() {
 	const [displayTime, setDisplayTime] = React.useState(25 * 60)
+	const [breakTime, setBreakTime] = React.useState(5 * 60)
 
 	const formatTime = (time) => {
 		let minutes = Math.floor(time / 60);
@@ -9,24 +10,31 @@ function App() {
 			(seconds < 10 ? '0' + seconds : seconds)
 	}
 
+	// Give arrow icon can modify braek length. todo; change pointer to hand icon
+	const changeTime = (amount, type) => {
+		if (type == 'break') {
+			setBreakTime( prev => prev + amount )
+		}
+	}
+
 	// display remaining time until break
 	return (
 		<div>
-			<Length title={'break length'} changetime={null} type={'break'} time={null} formatTime />
+			<Length title={'break length'} changeTime={changeTime} type={'break'} time={breakTime} formatTime={formatTime} />
 			<h1>{formatTime(displayTime)}</h1>
 		</div>
 	)
 
 	// Fixed missing html tags and babel calling `require` error. Adding length function to change break duration.
-	function Length({ title, changetime, type, time, formatTime }) {
+	function Length({ title, changeTime, type, time, formatTime }) {
 		return (
-			<div className='any'>
+			<div >
 				<h3>{title}</h3>
 				<div className='time-sets'>
-					<span className='material-symbols-outlined'>
+					<span className='material-symbols-outlined' onClick={() => changeTime(-60, type)}>
 						arrow_downward
 					</span>
-					<h3>formatTime{time}   </h3>
+					<h3>{formatTime(time)}   </h3>
 					<span className='material-symbols-outlined'>
 						arrow_upward
 					</span>
